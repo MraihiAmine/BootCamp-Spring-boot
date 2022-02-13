d58ed4f725a984093e2647b2eb63500a8e709a83
Mraihi Amine <mraihiamin@gmail.com>
2022-02-13 09:11:09 +0100

    Testing with mvc

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,16 +1,18 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
     @RequestMapping("/info")
-    public String info(){
+    public String info(Model model){
+        String training = "Fullstack";
+        model.addAttribute("workshop", training);
         return "home/info";
     }
-    @RequestMapping("/info")
+    @RequestMapping("/display")
     public String display(){
         return "home/display";
     }
